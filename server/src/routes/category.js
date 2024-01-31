6461512d58ed741d95ebebb089caebe0ccbd0e36
oleno <es.moname@gmail.com>
2024-01-31 16:51:01 +0100

    remove express middleware

--- server/src/routes/category.js
+++ server/src/routes/category.js
@@ -1,19 +1,7 @@
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 import { getCategories } from "../controllers/category.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const categoryRouter = express.Router();
-
-categoryRouter.use(
-  "/assets/categories",
-  express.static(
-    path.join(__dirname, "..", "..", "client", "public", "assets", "categories")
-  )
-);
 
 categoryRouter.get("/", getCategories);
 
