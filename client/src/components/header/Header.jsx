4f1c3e2b898732033131845e85a87823f852810d
mo92othman <mo92othman@gmail.com>
2024-02-07 18:01:02 +0100

    added add-item and profile-pic in header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "../../assets/search-icon.svg";
 import Logo from "../../assets/logo-color.svg";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import AddItemButton from "./AddItemButton";
+import ProfileDropdown from "./ProfileDropdown";
 
 function Header() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLogout = () => {
+    // Handle logout logic
+    setIsAuthenticated(!isAuthenticated);
+  };
+
   return (
     <header className="header">
-      <div className="logo">
+      <div className="logo" onClick={handleLogout}>
         <img src={Logo} alt="Logo" className="logo-image" />
       </div>
       <div className="header-content">
@@ -19,12 +28,21 @@ function Header() {
             className="search-input"
           />
         </div>
-        <Link to="/sign-in">
-          <button className="login-button">Sign In</button>
-        </Link>
-        <Link to="/sign-up">
-          <button className="get-started-button">Sign Up</button>
-        </Link>
+        {isAuthenticated ? (
+          <>
+            <AddItemButton />
+            <ProfileDropdown onLogout={handleLogout} />
+          </>
+        ) : (
+          <>
+            <Link to="/sign-in">
+              <button className="login-button">Sign In</button>
+            </Link>
+            <Link to="/sign-up">
+              <button className="get-started-button">Sign Up</button>
+            </Link>
+          </>
+        )}
       </div>
     </header>
   );
---
+++ client/src/components/header/ProfileDropdown.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./ProfileDropdown.css";
+import ProfilePic from "../../assets/fake-user.jpg";
+import PropTypes from "prop-types";
+
+const ProfileDropdown = ({ onLogout }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleLogout = () => {
+    if (onLogout) {
+      onLogout();
+    }
+    // Close the dropdown after logout
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="dropdown">
+      <div className="profile-picture" onClick={toggleDropdown}>
+        <img src={ProfilePic} alt="Profile" />
+      </div>
+      {isOpen && (
+        <ul className="dropdown-menu">
+          <li onClick={handleLogout}>Logout</li>
+          <li>We can add other options</li>
+          <li>And more options</li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
+ProfileDropdown.propTypes = {
+  onLogout: PropTypes.func.isRequired,
+};
+
+export default ProfileDropdown;
