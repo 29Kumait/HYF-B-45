c647b2267e5e0a2822eaf1b622a0c6895a9831bb
mo92othman <mo92othman@gmail.com>
2024-02-08 17:56:13 +0100

    add a check in the create user function

--- server/src/service/loginSignUp.js
+++ server/src/service/loginSignUp.js
@@ -1,10 +1,26 @@
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
+import { logError, logInfo } from "../util/logging.js";
 
 const createUser = async (userData) => {
-  const user = new User(userData);
-  await user.save();
-  return user;
+  try {
+    // Check if a user with the same email already exists
+    const existingUser = await User.findOne({ email: userData.email });
+
+    if (existingUser) {
+      logInfo("User with this email already exists:", existingUser);
+
+      throw new Error("User with this email already exists");
+    }
+
+    // No duplicate email, proceed to create a new user
+    const user = new User(userData);
+    await user.save();
+    return user;
+  } catch (error) {
+    logError("Error creating user:", error);
+    throw error;
+  }
 };
 
 const findUserByCredentials = async (username, password) => {
