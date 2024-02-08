b25c08f360c9158ab19c249e7d898b86997076ac
Rama <120380006+rama-website@users.noreply.github.com>
2024-02-08 16:50:31 +0100

    Adding Saerch functionality in the server-side

--- server/src/routes/item.js
+++ server/src/routes/item.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { getItems, createItem } from "../controllers/item.js";
 import { getItemAndUserDataById } from "../controllers/getItemAndUserDataById.js";
+import { searchItems } from "../controllers/item.js";
 
 const itemRouter = express.Router();
 
@@ -12,5 +13,8 @@ itemRouter.post("/", createItem);
 
 // Handle GET requests to retrieve a single item by its ID
 itemRouter.get("/:itemId", getItemAndUserDataById);
+
+// Add a new route for searching items
+itemRouter.get("/search", searchItems);
 
 export default itemRouter;
