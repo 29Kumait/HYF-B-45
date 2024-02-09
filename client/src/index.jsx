ddbfca6a41085163930f491ed9bfd829e522d146
sanaz <sanazzarei1995@gmail.com>
2024-02-09 14:16:45 +0100

    add popup

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { AuthProvider } from "./components/item/AuthProvider";
 import AppWrapper from "./AppWrapper";
 import App from "./App";
 
 ReactDOM.render(
   <AppWrapper>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </AppWrapper>,
   document.getElementById("root")
 );
