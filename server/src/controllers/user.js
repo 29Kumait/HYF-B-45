9aa483a2c7ce741b1463a8d71eb87390e34a54c8
mo92othman <mo92othman@gmail.com>
2024-02-12 18:17:20 +0100

    ädd a function to get the user by id in the server

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -46,3 +46,26 @@ export const createUser = async (req, res) => {
       .json({ success: false, msg: "Unable to create user, try again later" });
   }
 };
+
+export const getUserById = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ success: false, error: "User ID is required" });
+    }
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ success: false, error: "User not found" });
+    }
+
+    res.status(200).json({ success: true, result: user });
+  } catch (error) {
+    logError(error);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
+};
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { createUser, getUsers } from "../controllers/user.js";
+import { createUser, getUsers, getUserById } from "../controllers/user.js"; // Import the getUserById controller
 const userRouter = express.Router();
 
 userRouter.get("/", getUsers);
 userRouter.post("/create", createUser);
+
+// Define the route for getting a user by ID
+userRouter.get("/:userId", getUserById); // Define a route parameter :userId
 
 export default userRouter;
