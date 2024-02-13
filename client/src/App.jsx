f855a1cc7b8a3da13235bfcb240a0791317067d9
Kumait <Kumait@gmail.com>
2024-02-13 15:48:22 +0100

    <Route path="/rentPage" element={<RentPage />} />

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,9 @@ import Home from "./pages/Home/Home";
 import "./App.css";
 import PostItem from "./pages/PostItem/PostItem";
 import Item from "./pages/Item/Item.jsx";
+import RentPage from "./pages/RentPage/RentPage";
 import { AuthProvider } from "./components/Account/AuthContext";
+
 
 const App = () => {
   return (
@@ -14,6 +16,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/post-item" element={<PostItem />} />
           <Route path="/item/:itemId" element={<Item />} />
+          <Route path="/rentPage" element={<RentPage />} />
         </Routes>
       </AuthProvider>
     </>
