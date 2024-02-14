948d688a8201ae223232752756de0e417849d3a4
Kumait <Kumait@gmail.com>
2024-02-14 16:48:03 +0100

    Create Modal for Message new file:   server/src/models/Message.js

---
+++ server/src/models/Message.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  content: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
+});
+
+export const Message = mongoose.model("Message", messageSchema);
