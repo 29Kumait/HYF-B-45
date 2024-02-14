4da88f6a08e640a6a3164b596d7e3d675b544cbd
Kumait <Kumait@gmail.com>
2024-02-14 17:09:15 +0100

    add route messages to app.js app.use("/api/messages", messageRouter);

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-//import authRoutes from "./routes/authRoutes.js";
 import itemRouter from "./routes/item.js";
 import userRouter from "./routes/user.js";
 import categoryRouter from "./routes/category.js";
 import router from "./routes/authRoutes.js";
 import userInfoRouter from "./routes/userRoute.js";
 import expenseRouter from "./routes/expense.js";
+import messageRouter from "./routes/messageRouter.js";
 
 // Create an express server
 const app = express();
@@ -27,6 +27,7 @@ app.use("/api/category", categoryRouter);
 app.use("/api/item", itemRouter);
 app.use("/api", userInfoRouter);
 app.use("/api/user", userInfoRouter);
+app.use("/api/messages", messageRouter);
 app.use("/api/expense", expenseRouter);
 
 export default app;
