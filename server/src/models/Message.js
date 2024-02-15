95be8154d82a25337782e449ca4ee10896ef2bc3
Kumait <Kumait@gmail.com>
2024-02-15 01:08:10 +0100

    autofix +  delete  temporary test file.

--- server/src/models/Message.js
+++ server/src/models/Message.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
   sender: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
-  recipient: { type: mongoose.Schema.Types.ObjectId, ref: "users" },  
+  recipient: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
   content: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
 });
