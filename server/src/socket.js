72ab7bea34452982fe3d083735bc09011a96100f
Kumait <Kumait@gmail.com>
2024-02-19 16:23:52 +0100

    Refactor socket.js

--- server/src/socket.js
+++ server/src/socket.js
@@ -1,5 +1,6 @@
 import { Server as SocketIOServer } from "socket.io";
-import { logInfo } from "./util/logging.js";
+import { logInfo, logError } from "./util/logging.js";
+import { authenticateToken } from "./service/jwtChat.js";
 
 const initializeSocketIO = (server) => {
   const io = new SocketIOServer(server, {
@@ -9,21 +10,47 @@ const initializeSocketIO = (server) => {
     },
   });
 
-  io.on("connection", (socket) => {
-    // Handle joining a specific one-to-one chat
-    socket.on("join chat", (chatId) => {
-      socket.join(chatId);
-      logInfo(`User ${socket.id} joined chat ${chatId}`);
-    });
+  let users = {};
 
-    // Handle one-to-one chat message
-    socket.on("chat message", ({ chatId, msg }) => {
-      // Broadcast the message only to the other participant in the chat
-      socket.to(chatId).emit("chat message", msg);
+  io.on("connection", (socket) => {
+    const token = socket.handshake.query.token;
+    try {
+      const user = authenticateToken(token);
+      if (!user) throw new Error("Authentication failed.");
+      users[user.id] = { socketId: socket.id, username: user.username };
+      logInfo(`User ${user.username} connected with socket ${socket.id}`);
+    } catch (error) {
+      logError(`Authentication error: ${error.message}`);
+      socket.disconnect(true);
+      return;
+    }
+    socket.on("chat message", ({ recipientId, msg }) => {
+      try {
+        const userId = Object.keys(users).find(
+          (key) => users[key].socketId === socket.id
+        );
+        if (!userId) throw new Error("User not found or not authenticated");
+
+        const user = users[userId];
+        const recipientSocketId = users[recipientId]?.socketId;
+        if (!recipientSocketId) throw new Error("Recipient not connected.");
+
+        io.to(recipientSocketId).emit("chat message", {
+          from: user.username,
+          message: msg,
+        });
+      } catch (error) {
+        logError(`Chat message error: ${error.message}`);
+        socket.emit("error", { message: error.message });
+      }
     });
 
     socket.on("disconnect", () => {
-      logInfo(`User disconnected: ${socket.id}`);
+      const userId = Object.keys(users).find((key) => users[key] === socket.id);
+      if (userId) {
+        logInfo(`User ID ${userId} disconnected`);
+        delete users[userId];
+      }
     });
   });
 
