071b4a1d2778414a7c0513f60449bacbe2c0b167
Kumait <Kumait@gmail.com>
2024-02-19 20:55:03 +0100

    Add SideChat component

---
+++ client/src/components/chat/SideChat.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+import useOutsideClick from "../../hooks/useOutsideClick.js";
+import "./chatStyle.css";
+
+const SideChat = ({ open, onClose }) => {
+  const [showSide, setShowSide] = useState(false);
+  const sideRef = useRef(null);
+
+  useEffect(() => {
+    setShowSide(open);
+  }, [open]);
+
+  const closeSidebar = () => {
+    setShowSide(false);
+    if (onClose) onClose(); // Call the onClose prop function when sidebar is closed
+  };
+
+  useOutsideClick(sideRef, closeSidebar);
+
+  return <>{showSide && <div ref={sideRef} className="sidebar"></div>}</>;
+};
+
+SideChat.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func,
+};
+
+export default SideChat;
