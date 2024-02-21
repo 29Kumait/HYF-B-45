9fbd2c0dedc39f02b8c05dcd270db414644e6db2
oleno <es.moname@gmail.com>
2024-02-21 21:39:23 +0100

    remove upload button

--- client/src/components/postItem/UploadImages.jsx
+++ client/src/components/postItem/UploadImages.jsx
@@ -4,11 +4,10 @@ import PropTypes from "prop-types";
 
 const UploadImages = ({ handleImageUpload }) => {
   const [imageSrc, setImageSrc] = useState();
-  const [uploadImage, setUploadImage] = useState();
   const [isUploading, setIsUploading] = useState(false);
   const [error, setError] = useState(null);
 
-  const handleOnChange = (changeEvent) => {
+  const handleOnChange = async (changeEvent) => {
     const file = changeEvent.target.files[0];
 
     if (file) {
@@ -19,71 +18,51 @@ const UploadImages = ({ handleImageUpload }) => {
         setImageSrc(null);
         return;
       }
-    }
 
-    const reader = new FileReader();
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("upload_preset", "user-uploads");
 
-    reader.onload = (e) => {
-      setImageSrc(e.target.result);
-      setUploadImage(undefined);
-    };
-    reader.readAsDataURL(file);
-  };
+      setIsUploading(true);
+      setError(null);
 
-  const handleOnSubmit = async (e) => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    const fileInput = Array.from(form.elements).find(
-      ({ name }) => name === "file"
-    );
-
-    const formData = new FormData();
-    formData.append("file", fileInput.files[0]);
-    formData.append("upload_preset", "user-uploads");
-
-    setIsUploading(true);
-    setError(null);
-
-    try {
-      const response = await fetch(
-        "https://api.cloudinary.com/v1_1/dibnqoge8/image/upload",
-        {
-          method: "POST",
-          body: formData,
+      try {
+        const response = await fetch(
+          "https://api.cloudinary.com/v1_1/dibnqoge8/image/upload",
+          {
+            method: "POST",
+            body: formData,
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Failed to upload image. Please try again later.");
         }
-      );
-      if (!response.ok) {
-        throw new Error("Failed to upload image. Please try again later.");
+        const data = await response.json();
+        setImageSrc(data.secure_url);
+        handleImageUpload(data.secure_url);
+      } catch (error) {
+        setError(`Error uploading image: ${error.message}`);
+      } finally {
+        setIsUploading(false);
       }
-      const data = await response.json();
-      setImageSrc(data.secure_url);
-      setUploadImage(data);
-      handleImageUpload(data.secure_url);
-    } catch (error) {
-      setError(`Error uploading image: ${error.message}`);
-    } finally {
-      setIsUploading(false);
     }
   };
 
   return (
-    <form method="post" onChange={handleOnChange} onSubmit={handleOnSubmit}>
+    <form method="post">
       <p>
-        <input className="file-input" type="file" name="file" />
+        <input
+          className="file-input"
+          type="file"
+          name="file"
+          onChange={handleOnChange}
+        />
       </p>
       <div className="image-container">
         <img className="upload-image" src={imageSrc} />
       </div>
       {isUploading && <p className="notification">Uploading image...</p>}
       {error && <p className="notification">{error}</p>}
-      {imageSrc && !uploadImage && (
-        <p className="button-container">
-          <button className="upload-button">Upload Image</button>
-        </p>
-      )}
-      {imageSrc && uploadImage && !isUploading && (
-        <p className="notification">Image uploaded successfully</p>
-      )}
     </form>
   );
 };
