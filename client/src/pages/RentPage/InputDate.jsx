8befecc20dd5a0327530143216234a4959a0297a
oleno <es.moname@gmail.com>
2024-02-21 11:16:27 +0100

    add date-picker

--- client/src/pages/RentPage/InputDate.jsx
+++ client/src/pages/RentPage/InputDate.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import useFetch from "../../hooks/useFetch";
 import { logInfo, logError } from "../../../../server/src/util/logging";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 const InputDate = ({
   handleStartDateChange,
   handleEndDateChange,
@@ -31,27 +34,31 @@ const InputDate = ({
     return cancelFetch;
   }, [isRentPageLoaded, performFetch, cancelFetch]);
 
-  const handleStartDateInput = (e) => {
-    const selectedStartDate = e.target.value;
-    setStartDate(selectedStartDate);
-    handleStartDateChange(selectedStartDate);
+  const handleStartDateInput = (date) => {
+    setStartDate(date);
+    handleStartDateChange(date);
   };
 
-  const handleEndDateInput = (e) => {
-    const selectedEndDate = e.target.value;
-    setEndDate(selectedEndDate);
-    handleEndDateChange(selectedEndDate);
+  const handleEndDateInput = (date) => {
+    setEndDate(date);
+    handleEndDateChange(date);
   };
 
   return (
     <div>
       <div>
         <label>Start Date:</label>
-        <input type="date" value={startDate} onChange={handleStartDateInput} />
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => handleStartDateInput(date)}
+        />
       </div>
       <div>
         <label>End Date:</label>
-        <input type="date" value={endDate} onChange={handleEndDateInput} />
+        <DatePicker
+          selected={endDate}
+          onChange={(date) => handleEndDateInput(date)}
+        />
       </div>
     </div>
   );
