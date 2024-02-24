d39fc7ab8a80f80dadaea27ca92bac7b5ae3f104
oleno <es.moname@gmail.com>
2024-02-24 17:19:32 +0100

    add dropdown associated with notification

--- client/src/components/header/Message.jsx
+++ client/src/components/header/Message.jsx
@@ -4,11 +4,15 @@ import "./Message.css";
 import { useAuth } from "../Account/AuthContext";
 import MessageIcon from "../../assets/message.svg";
 import io from "socket.io-client";
+import { useNavigate } from "react-router-dom";
 
 const Message = () => {
   const { userData } = useAuth();
-  const [listedItemsIds, setListedItemsIds] = useState([]);
+  const [listedItems, setListedItems] = useState([]);
   const [notificationCount, setNotificationCount] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+  const [itemsIdWithNotifications, setItemsIdWithNotifications] = useState([]);
+  const navigate = useNavigate();
 
   // Check if userData exists before performing the fetch
   useEffect(() => {
@@ -23,8 +27,7 @@ const Message = () => {
             `${process.env.BASE_SERVER_URL}/api/transactions/${userId}`
           );
           const data = await response.json();
-          const ids = data.listedItems.map((item) => item._id);
-          setListedItemsIds(ids);
+          setListedItems(data.listedItems);
         } catch (error) {
           // console.error("Error fetching transactions:", error);
         }
@@ -41,8 +44,12 @@ const Message = () => {
     socket.on("notification", (message) => {
       const itemId = message.split("-")[1]; // Extract the item ID from the message
 
-      // Check if the extracted item ID is in listedItemsIds
-      if (listedItemsIds.includes(itemId)) {
+      if (!itemsIdWithNotifications.includes(itemId)) {
+        // Include item id related to the notification in the array
+        setItemsIdWithNotifications((prevItemsIds) => [
+          ...prevItemsIds,
+          itemId,
+        ]);
         // Increment notification count
         setNotificationCount((prevCount) => prevCount + 1);
       }
@@ -52,16 +59,36 @@ const Message = () => {
     return () => {
       socket.disconnect();
     };
-  }, [listedItemsIds]);
+  }, [itemsIdWithNotifications]);
+
+  const toggleDropdown = () => {
+    setIsOpen((prevIsOpen) => !prevIsOpen);
+  };
+
+  const handleSelectedItem = (itemId) => {
+    navigate(`/item/${itemId}`);
+    toggleDropdown();
+  };
 
   return (
     <div className="message-container">
-      <div className="add-item-icon-container">
+      <div className="add-item-icon-container" onClick={toggleDropdown}>
         <img src={MessageIcon} alt="Message" className="add-item-icon" />
         {notificationCount > 0 && (
           <div className="notification-count">{notificationCount}</div>
         )}
       </div>
+      {isOpen && (
+        <ul className="dropdown-menu">
+          {listedItems
+            .filter((item) => itemsIdWithNotifications.includes(item._id))
+            .map((item) => (
+              <li key={item._id} onClick={() => handleSelectedItem(item._id)}>
+                {item.title}
+              </li>
+            ))}
+        </ul>
+      )}
     </div>
   );
 };
