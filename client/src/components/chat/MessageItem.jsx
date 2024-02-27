2ed5cc993b97ac3552b470a150870935074d032f
Kumait <Kumait@gmail.com>
2024-02-27 05:45:22 +0100

    Add MessageItem component to handle individual chat messages

---
+++ client/src/components/chat/MessageItem.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import FakeUserProfilePicture from "../../assets/fake-user.jpg";
+
+const MessageItem = ({ message }) => (
+  <li className="message-item">
+    <div>
+      <img
+        src={message.pic || FakeUserProfilePicture}
+        alt="profile-pic"
+        className="chat-profile-pic"
+      />
+    </div>
+    <div className="message-info">
+      <strong className="chat-strong">{message.userName}</strong>
+      <span className="message-time">{message.time}</span>
+      <div className="message-text">{message.text}</div>
+    </div>
+  </li>
+);
+
+MessageItem.propTypes = {
+  message: PropTypes.shape({
+    pic: PropTypes.string,
+    userName: PropTypes.string.isRequired,
+    time: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default MessageItem;
