700e0311aa5e09a06d64ed3c950932112ef8b610
mo92othman <mo92othman@gmail.com>
2024-02-27 13:16:23 +0100

    send a notification only to the owner

--- client/src/components/header/Message.jsx
+++ client/src/components/header/Message.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./Message.css";
-// import useFetch from "../../hooks/useFetch";
 import { useAuth } from "../Account/AuthContext";
 import MessageIcon from "../../assets/message.svg";
 import io from "socket.io-client";
@@ -14,13 +13,11 @@ const Message = () => {
   const [itemsIdWithNotifications, setItemsIdWithNotifications] = useState([]);
   const navigate = useNavigate();
 
-  // Check if userData exists before performing the fetch
   useEffect(() => {
     if (userData && userData.user) {
       const { user } = userData;
       const userId = user._id;
 
-      // Perform fetch when userData is available
       const fetchTransactions = async () => {
         try {
           const response = await fetch(
@@ -37,29 +34,28 @@ const Message = () => {
     }
   }, [userData]);
 
-  // Establish socket connection and handle notifications
   useEffect(() => {
     const socket = io(process.env.BASE_SERVER_URL);
 
     socket.on("notification", (message) => {
       const itemId = message.split("-")[1]; // Extract the item ID from the message
 
-      if (!itemsIdWithNotifications.includes(itemId)) {
-        // Include item id related to the notification in the array
+      // Check if the item belongs to the current user
+      const itemBelongsToUser = listedItems.some((item) => item._id === itemId);
+
+      if (itemBelongsToUser && !itemsIdWithNotifications.includes(itemId)) {
         setItemsIdWithNotifications((prevItemsIds) => [
           ...prevItemsIds,
           itemId,
         ]);
-        // Increment notification count
         setNotificationCount((prevCount) => prevCount + 1);
       }
     });
 
-    // Clean up the socket connection when the component unmounts
     return () => {
       socket.disconnect();
     };
-  }, [itemsIdWithNotifications]);
+  }, [listedItems, itemsIdWithNotifications]);
 
   const toggleDropdown = () => {
     setIsOpen((prevIsOpen) => !prevIsOpen);
