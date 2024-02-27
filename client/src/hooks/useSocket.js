9272b57821b68ccd04dc37cea345331b6bb59f3a
Kumait <Kumait@gmail.com>
2024-02-27 02:38:56 +0100

    Add useSocket hook for socket.io integration

---
+++ client/src/hooks/useSocket.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+
+const useSocket = (url) => {
+  const [socket, setSocket] = useState(null);
+  useEffect(() => {
+    const newSocket = io(url);
+
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, [url]);
+
+  const emitEvent = (eventName, data) => {
+    if (socket) {
+      socket.emit(eventName, data);
+    }
+  };
+
+  return { socket, emitEvent};
+};
+
+export default useSocket;
